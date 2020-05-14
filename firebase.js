7c4c173b8689467a784abd88cd9b6c36fae689d7
mbcheung <mbcheung@students.cs.ubc.ca>
2020-05-14 12:31:53 -0600

    Added data persistence through Firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyAx9OcpCXFHqXA90BpnUIlgsCUW0Bs49xs",
+  authDomain: "library-5f09a.firebaseapp.com",
+  databaseURL: "https://library-5f09a.firebaseio.com",
+  projectId: "library-5f09a",
+  storageBucket: "library-5f09a.appspot.com",
+  messagingSenderId: "386977404819",
+  appId: "1:386977404819:web:e8d9bc3589a63a3c6a3a66"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- library.js
+++ library.js
@@ -1,6 +1,4 @@
-
 const button = document.getElementsByTagName("button")[0];
-// const bookView = document.getElementsByClassName("book-view")[0]; //fix
 const titleField = document.getElementsByName("title")[0];
 const authorField = document.getElementsByName("author")[0];
 const pagesField = document.getElementsByName("pages")[0];
@@ -10,18 +8,49 @@ const bookViews = document.getElementsByClassName("book-view");
 const parser = new DOMParser();
 const readBookView = document.getElementById("tab-read");
 const unreadBookView = document.getElementById("tab-unread");
-let myLibrary = [];
+let myLibrary = {};
+
+firebase.database().ref('/books/').once('value').then(function(snapshot) {
+  let allData = snapshot.val();
+  for (let key in allData) {
+    let data = allData[key];
+    let book = new Book(data.title, data.author, data.pages, data.isRead);
+    let bookCard = book.constructElement();
+    bookCard.key = key;
+    book.key = key;
+    if (book.isRead) { // switch on type
+      readBookView.appendChild(bookCard);
+    } else {
+      unreadBookView.appendChild(bookCard);
+    }
+    myLibrary[key] = book;
+  }
+});
 
 function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
-  this.element = this.constructElement();
+}
+
+function constructDeleteButton() {
+  let deleteButton = document.createElement("IMG");
+  deleteButton.className = "delete";
+  deleteButton.src = "./deleteBtn.png"
+  deleteButton.title = "delete book";
+  return deleteButton;
+}
+
+function constructReadButton() {
+  let readButton = document.createElement("IMG");
+  readButton.className = "readBtn";
+  readButton.src = "./empty-books.png";
+  readButton.title = "change read status";
+  return readButton;
 }
 
 Book.prototype.constructElement = function () {
-  // TODO: refactor lol
   let bookCard = document.createElement("DIV");
   bookCard.className = "book-card";
   bookCard.setAttribute("read", this.isRead);
@@ -31,15 +60,8 @@ Book.prototype.constructElement = function () {
                     <br/>${this.pages} PAGES
                   </div>`;
   bookCard.innerHTML = string;
-  bookCard.book = this;
-  let deleteButton = document.createElement("IMG");
-  deleteButton.className = "delete";
-  deleteButton.src = "./deleteBtn.png"
-  deleteButton.title = "delete book"
-  let readButton = document.createElement("IMG");
-  readButton.className = "readBtn";
-  readButton.src = "./empty-books.png";
-  readButton.title = "change read status"
+  let deleteButton = constructDeleteButton();
+  let readButton = constructReadButton();
   bookCard.prepend(deleteButton);
   bookCard.appendChild(readButton);
   bindDeleteEvent(deleteButton, bookCard);
@@ -48,35 +70,53 @@ Book.prototype.constructElement = function () {
 }
 
 function deleteBookCard(bookCard) {
-  if (bookCard.book.isRead) { //switch on type
+  if (bookCard.getAttribute("read") === "true") { //switch on type
     readBookView.removeChild(bookCard);
   } else {
     unreadBookView.removeChild(bookCard);
   }
 }
 
+function deleteBookFromDatabase(key) {
+  let bookRef = firebase.database().ref('books/' + key);
+  bookRef.remove();
+}
+
 function bindDeleteEvent(button, bookCard) {
   button.addEventListener("click", () => {
-    let idx = myLibrary.indexOf(bookCard.book);
-    myLibrary.splice(idx, 1);
+    delete myLibrary[bookCard.key];
     deleteBookCard(bookCard);
+    deleteBookFromDatabase(bookCard.key);
   });
 }
 
 function addBookCard(bookCard) {
-  if (bookCard.book.isRead) { //switch on type
-    readBookView.appendChild(bookCard);
+  let bookView;
+  if (bookCard.getAttribute("read") === "true") { //switch on type
+    bookView = readBookView;
   } else {
-    unreadBookView.appendChild(bookCard);
+    bookView = unreadBookView;
   }
+  bookView.appendChild(bookCard);
+  return bookView.className.includes("active");
+}
+
+function updateBookOnDatabase(key) {
+  let book = myLibrary[key];
+  let updates = {};
+  updates['/books/' + key] = book;
+  firebase.database().ref().update(updates);
 }
 
 function bindToggleEvent(button, bookCard) {
   button.addEventListener("click", () => {
     deleteBookCard(bookCard);
-    bookCard.book.isRead = !bookCard.book.isRead;
-    bookCard.setAttribute("read", bookCard.book.isRead);
+    let isRead = myLibrary[bookCard.key].isRead; 
+    myLibrary[bookCard.key].isRead = !isRead;
+    bookCard.setAttribute("read", !isRead);
+    updateBookOnDatabase(bookCard.key);
     addBookCard(bookCard);
+    toggleTabsAndBookViews();
   })
 }
 
@@ -92,15 +132,26 @@ function getBook() {
   return new Book(title, author, pages, isRead);
 }
 
+function postToDatabase(book) {
+  let newBookKey = firebase.database().ref().child('books').push().key;
+  let updates = {};
+  updates['/books/' + newBookKey] = book;
+  firebase.database().ref().update(updates);
+  return newBookKey;
+}
+
+
 function addBookToLibrary() {
   let book = getBook();
   if (book) {
-    if (book.isRead) {
-      readBookView.appendChild(book.element);
-    } else {
-      unreadBookView.appendChild(book.element);
+    let key = postToDatabase(book);
+    let bookCard = book.constructElement();
+    bookCard.key = key;
+    book.key = key;
+    if (!addBookCard(bookCard)){
+      toggleTabsAndBookViews();
     }
-    myLibrary.push(book);
+    myLibrary[key] = book;
   }
 }
 
